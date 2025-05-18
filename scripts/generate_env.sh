4e84a0ec619ada56753b781a26e65abe3a36c074
shigenori.terada <shigenori.terada@gmail.com>
2025-05-18 14:15:48 +0900

    #1 delirekutori

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -5,6 +5,9 @@ set -e
 export URL_IMPORT_FF_API=$(aws ssm get-parameter --name "URL_IMPORT_FF_API" --with-decryption --query "Parameter.Value" --output text --region ap-northeast-1)
 export S3_BUCKET_FULFILLMENT=$(aws ssm get-parameter --name "S3_BUCKET_FULFILLMENT" --with-decryption --query "Parameter.Value" --output text --region ap-northeast-1)
 
+# config ディレクトリがなければ作成
+mkdir -p /var/www/html/config
+
 cat << EOF > /var/www/html/config/environments.php
 <?php
 /**
