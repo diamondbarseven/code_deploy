ce17d2089093bff2af6288dd74ec9e9ab80e474a
shigenori.terada <shigenori.terada@gmail.com>
2025-05-18 13:32:09 +0900

    #1 code deploy エラーの修正

--- scripts/embed_env.sh
+++ scripts/embed_env.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# SSMからパラメータを取得（例: /myapp/env/MESSAGE というキー）
 MESSAGE=$(aws ssm get-parameter --name "/MESSAGE" --with-decryption --query "Parameter.Value" --output text --region ap-northeast-1)
-
-# HTMLファイルのコピーをテンプレートとして編集
 cp /var/www/html/index.html /var/www/html/index.html.bak
-
-# プレースホルダー __MESSAGE__ を環境変数の値で置き換え
 sed -i "s|__MESSAGE__|$MESSAGE|g" /var/www/html/index.html
