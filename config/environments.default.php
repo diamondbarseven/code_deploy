c74ce0f101262f446f1c020b43f724fe5ee09770
shigenori.terada <shigenori.terada@gmail.com>
2025-05-18 14:11:05 +0900

    #1 environments でテスト

---
+++ config/environments.default.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Setting for fulfillment
+*/
+//
+define('URL_IMPORT_FF_API', 'https://192.168.0.8:14080/pms/admin/v1/items/import');
+define('S3_BUCKET_FULFILLMENT', 'product-master-develop');
---
+++ scripts/generate_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# AWS CLIが使える環境が前提
+export URL_IMPORT_FF_API=$(aws ssm get-parameter --name "URL_IMPORT_FF_API" --with-decryption --query "Parameter.Value" --output text --region ap-northeast-1)
+export S3_BUCKET_FULFILLMENT=$(aws ssm get-parameter --name "S3_BUCKET_FULFILLMENT" --with-decryption --query "Parameter.Value" --output text --region ap-northeast-1)
+
+cat << EOF > /var/www/html/config/environments.php
+<?php
+/**
+ * Setting for fulfillment
+ */
+define('URL_IMPORT_FF_API', '${URL_IMPORT_FF_API}');
+define('S3_BUCKET_FULFILLMENT', '${S3_BUCKET_FULFILLMENT}');
+EOF
